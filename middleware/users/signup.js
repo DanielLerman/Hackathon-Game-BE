d99e993869d5279bfda7b81ecf8b209165f11d70
Amitai Zand <amitai17@gmail.com>
2023-01-30 12:51:39 +0200

    Login routing, middleware (incl. validation), models (except for DB), and controller complete

--- middleware/users/signup.js
+++ middleware/users/signup.js
@@ -1,7 +1,7 @@
 const { signupSchema } = require("../validation/schemas");
 const compileValidationSchema = require("../validation/compileValidationSchema");
 const hash = require("../../lib/bcrypt");
-const checkDBForSameEmailAndNickname = require("../../models/users/checkDBForSameEmailAndNickname");
+const checkDBForExistingEmailAndNickname = require("../../models/users/checkDBForExistingEmailAndNickname");
 
 function validateSignup(req, res, next) {
     const validate = compileValidationSchema(signupSchema);
@@ -23,17 +23,23 @@ function doPasswordsMatch(req, res, next) {
     return next(err);
 }
 
-function areEmailAndNicknameUnique(req, res, next) {
-    const areUnique = checkDBForSameEmailAndNickname(
-        req.body.email,
-        req.body.nickname
-    );
-    if (areUnique) return next();
-    const err = new Error();
-    err.statusCode = 400;
-    err.message =
-        "There is already a user with that email address and/or nickname.";
-    return next(err);
+async function areEmailAndNicknameUnique(req, res, next) {
+    try {
+        const areUnique = await checkDBForExistingEmailAndNickname(
+            req.body.email,
+            req.body.nickname
+        );
+        if (areUnique) return next();
+        const err = new Error();
+        err.statusCode = 400;
+        err.message =
+            "There is already a user with that email address and/or nickname.";
+        return next(err);
+    } catch (err) {
+        err.statusCode = 500;
+        err.message = "There was an error looking up the user in the database.";
+        return next(err);
+    }
 }
 
 function hashPassword(req, res, next) {
--- models/users/checkDBForSameEmailAndNickname.js
+++
@@ -1,5 +0,0 @@
-async function checkDBForSameEmailAndNickname(email, nickname) {
-    // Add Mongo or MySQL query here
-}
-
-module.exports = checkDBForSameEmailAndNickname;
