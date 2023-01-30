2f642d92c71c1798f04c093e480ebbc027599769
Amitai Zand <amitai17@gmail.com>
2023-01-30 15:20:22 +0200

    Set up empty scores routes

--- routers/scores.js
+++ routers/scores.js
@@ -1,4 +1,11 @@
 const express = require("express");
 const scores = express.Router();
 
+scores.get(); // Get All Users Score History API
+scores.post(); // Add Score API
+
+scores.get("/:id"); // Get Logged In User’s Score history API (Protected to logged in user only)
+scores.get("/last/:id"); // Get user last (latest) score API (Protected to logged in user only)
+scores.get("/high/:id"); // Get user highest score API (Protected to logged in user only)
+
 module.exports = scores;
